454b1e7af4bfcbe762d165a36d47e20158a42a28
Samantha Jaromirski <samanthajaromirski@gmail.com>
2014-09-24 20:31:24 -0400

    Sorcery authentication complete

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,12 @@
 class TagsController < ApplicationController
+  before_filter :require_login, only: [:destroy]
 
   def show
     @tag = Tag.find(params[:id])
   end
 
   def index 
-    @tags = Tags.all
+    @tags = Tag.all
   end
   
 end
